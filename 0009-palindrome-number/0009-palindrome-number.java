25339b3a82bf7ec6c3d4f6cf51de2d1783d3982e
김세영 <kimseyeong97@gmail.com>
2022-12-03 02:52:55 +0900

    Time: 9 ms (97.67%), Space: 41.5 MB (95.15%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,8 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
         
-        String origin = String.valueOf(x);
-        String reverse = new StringBuilder(origin).reverse().toString();
-        return origin.equals(reverse);
+        if (x < 0) {
+            return false;
+        }
+        
+        int tmp = x;
+        int reverse = 0;
+        while (tmp != 0) {
+            reverse = reverse * 10 + tmp % 10;
+            tmp /= 10;
+        }
+        
+        return x == reverse;
     }
 }
