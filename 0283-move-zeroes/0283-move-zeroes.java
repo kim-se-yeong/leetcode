39b6f0d95c28b73ba573e35c02f4cddc15b6cdc9
김세영 <kimseyeong97@gmail.com>
2022-12-04 20:54:12 +0900

    Time: 12 ms (24.30%), Space: 54.3 MB (75.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,13 +3,12 @@ class Solution {
         
         int index = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (Math.abs(nums[i]) > 0) {
-                nums[index++] = nums[i];
+            if (nums[i] != 0) {
+                int tmp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = tmp;
+                index++;
             }
-        }
-
-        while (index < nums.length) {
-            nums[index++] = 0;
         }
     }
 }
